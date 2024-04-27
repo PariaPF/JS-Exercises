efc3b6e4e98061b2e2049272948a529aaff1e5e8
Paria_Pileforoushha <pariapileforooshha@gmail.com>
2024-04-27 19:22:15 +0330

    8th exercise

---
+++ no 8/index.js
@@ -0,0 +1,16 @@
+const maxFunction = (num1, num2, num3) => {
+  let maxNum;
+  if (num1 >= num2) {
+    maxNum = num1;
+  } else {
+    maxNum = num2;
+  }
+
+  if (num3 >= maxNum) {
+    maxNum = num3;
+  }
+
+  return maxNum;
+};
+
+console.log(maxFunction(-2, 0, 1 / 4));
